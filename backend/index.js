27dbd49a51bd7893db625133b4446e5102511fa7
Subhransu parida G <subhransuparida35@gmail.com>
2025-10-03 11:21:20 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import geminiResponse from "./gemini.js"
 
 const app=express()
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["https://virtual-assistance-fkrj.onrender.com"],
     credentials:true
 }))
 const port=process.env.PORT || 5000
